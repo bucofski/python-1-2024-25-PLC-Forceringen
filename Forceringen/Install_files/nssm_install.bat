51977e26e52374304539dc771dd8afca0d6a1ac2
bucofski <tom_vandevyver@telenet.be>
2025-06-17 14:40:15 +0200

    Update nssm installation script and run batch file for Forceringen application

--- Forceringen/Install_files/nssm_install.bat
+++ Forceringen/Install_files/nssm_install.bat
@@ -1,15 +1,15 @@
 @echo You must run this as administrator.  If you forgot, press CTRL+C
 pause
  
-set DASHBOARD_NAME=demo_dashboard
+set APP_NAME=Forceringen
  
-nssm remove %DASHBOARD_NAME% confirm
+nssm remove %APP_NAME% confirm
 pause
  
-nssm install %DASHBOARD_NAME% "C:\shiny-board\_config\demo_dashboard\run.bat"
-nssm set %DASHBOARD_NAME% DisplayName "Demo Dashboard"
-nssm set %DASHBOARD_NAME% Description "This service runs the demo dashboard application."
-nssm set %DASHBOARD_NAME% ObjectName "DOMAIN\USERNAME" "PASSWORD"
-nssm set %DASHBOARD_NAME% AppExit 1 Restart
-nssm start %DASHBOARD_NAME%
+nssm install %APP_NAME% "C:\PythonApp\Forcerigen\Install_files\run.bat"
+nssm set %APP_NAME% DisplayName "Forceringen"
+nssm set %APP_NAME% Description "This service runs the Forceringen application."
+nssm set %APP_NAME% ObjectName "DOSIM000\KWA.BT2.SERVERS" "Arcelor1"
+nssm set %APP_NAME% AppExit 1 Restart
+nssm start %APP_NAME%
 pause
--- Forceringen/Install_files/run.bat
+++ Forceringen/Install_files/run.bat
@@ -1,53 +1,21 @@
-:: Windows batch file to start the application.  Can be used with nssm.exe to create a Windows service
-:: vindevoy - 2025-04-08
- 
-::@echo off
-::set ENV_FILE=../../shinyboard/config/prysent.env
- 
-::
-:: DO NOT TOUCH THE CODE BELOW
-::::::::::::::::::::::::::::::
- 
-:: Load all the environment variables from the .env file
-::for /f "usebackq tokens=1,2 delims== eol=#" %%A in ("%ENV_FILE%") do (
-::    set "%%A=%%B"
-::)
- 
-:: Create the logging directory if it does not exist.  You get an error from Python if it does not exist.
-::IF NOT EXIST %SHINYBOARD_CONFIG% (
-::    mkdir %SHINYBOARD_CONFIG%
-::)
- 
+call activate am2412_tovy_server
+
 :: Check if the port is in use
 setlocal enabledelayedexpansion
  
-netstat -ano | find ":%SHINYBOARD_PORT%" >nul 2>&1
+netstat -ano | find ":8000" >nul 2>&1
  
 if not errorlevel 1 (
-    echo Port %SHINYBOARD_PORT% is already in use.  Find another port to start this dashboard.
+    echo Port "8000" is already in use.  Find another port to start this dashboard.
     pause
     exit /b 1
 )
  
-:: Activate python environment and start the server
-CALL activate %SHINYBOARD_ENV%
- 
 :: Start the application with uvicorn, watching specify directories and files
 uvicorn main:app ^
     --host 0.0.0.0 ^
-    --port %SHINYBOARD_PORT% ^
-    --app-dir=%SHINYBOARD_HOME% ^
-    --reload ^
-    --reload-delay=0 ^
-    --reload-dir=%SHINYBOARD_ROOT% ^
-    --reload-dir=%SHINYBOARD_CONFIG% ^
-    --reload-dir=%SHINYBOARD_HOME% ^
-    --reload-include=*.md ^
-    --reload-include=*.py ^
-    --reload-include=*.svg ^
-    --reload-include=config.yaml ^
-    --timeout-graceful-shutdown 0 ^
+    --port "8000" ^
+    --app-dir="C:\PythonApp\Forceringen\ui" ^
     --no-access-log
- 
- 
+  
 pause
